6cb93d1ee5469631d49e0f1bb4bfdfd931cc345c
Sachin <rahulit1708@gmail.com>
2024-07-02 00:10:30 +0530

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+//instance of mongoose
+const mongoose=require("mongoose");
+require("dotenv").config();
+
+exports.connect = ()=>{
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(()=>console.log("DB connected successfully"))
+    .catch((error)=>{
+        console.log("DB connection failed");
+        console.log(error);
+        process.exit(1);
+    })
+}
+//database se connect using mongoose
